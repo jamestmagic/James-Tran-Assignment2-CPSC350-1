51b88c59f369baabe43e450bbb22c8e660eefba7
root <jamestranformal@gmail.com>
2019-09-27 21:04:39 +0000

    Grid

--- Generation.cpp
+++ Generation.cpp
@@ -9,12 +9,26 @@
 
 using namespace std;
 
-
+char** myGrid;
 Generation::Generation()//constructor
 {
-
+  myGrid = new char*[0];
+  myGrid[0] = new char[0];
+}
+Generation::Generation(int row, int col){//overloaded constructor
+  myGrid = new char*[row];
+  for(int i = 0;i < row;++i){
+    myGrid[i] = new char[col];
+  }
 }
 Generation::~Generation()//destructor
 {
   cout << "object deleted" << endl;
+}
+
+bool isEmpty(int row, int col){
+  if(myGrid[row][col] == '-'){
+    return true;
+  }
+  return false;
 }
--- Generation.h
+++ Generation.h
@@ -4,7 +4,9 @@ using namespace std;
 
 class Generation{
   public:
+    char** myGrid;
     Generation();
+    Generation(int row, int col);
     ~Generation();
     void printGen(); //prints the generation in whatever
 
